0b5916d56510d25a7489d792cb1cb64b3a4db709
delfofe <facundotrionfetti@gmail.com>
2026-05-22 12:49:34 -0300

    Version 1.0: suma de argumentos

---
+++ prueba.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <num1> <num2>\n", argv[0]);
+        return 1;
+    }
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int sum = a + b;
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+    return 0;
+}
